520727db08d82f7ea611b0bfbab7383f07ad7f56
Gabriel Alfredo Siguenza <gebsiguenza@gmail.com>
2022-04-05 18:04:01 -0700

    Add files via upload

---
+++ CoffeeMaker.java
@@ -0,0 +1,5 @@
+package com.company;
+
+public interface CoffeeMaker {
+    public void makeCoffee();
+}
---
+++ Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+/**
+ * Main Driver for CMM Lab Assignment
+ *
+ */
+public class Main {
+
+    //TODO: Create instance of CMM class here and test functions
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
