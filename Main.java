d441b652ff87231961228fe3bc2fe3092bd7ed02
gsiguenza12 <gebsiguenza@gmail.com>
2022-04-06 23:37:33 -0700

    Tested regular coffee program selection, calling getName() method as well as setting environment and using getEnvironment() for Coffee_Server, makeCoffee() method. Output looks correct.

--- Main.java
+++ Main.java
@@ -8,8 +8,6 @@ public class Main {
 
 
     public static void main(String[] args) {
-
-
 	// write your code here
         System.out.println("The main is running...");
 
@@ -17,7 +15,6 @@ public class Main {
 
         // testing out program selection
         CMMOperatingSystem InstanceOS = new CMMOperatingSystem();
-
 
         InstanceOS.run("coffee");
     }
